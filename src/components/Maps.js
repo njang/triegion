1359fb206f5de9e9aab00d90d9af5b126765d2e5
njang <neo.w.jang@gmail.com>
2018-02-14 11:10:03 -0600

    Remove react-google-maps

---
+++ src/components/Maps.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { GoogleMap, Marker } from "react-google-maps"
+
+class Maps extends Component {
+  render() {
+    return (
+			<div>
+				MapComponent
+			</div>
+    );
+  }
+}
+
+export default Maps;        
